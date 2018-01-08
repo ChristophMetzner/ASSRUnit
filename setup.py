f28b8a410f63809cbd92bba32873277022046c7b
ChristophMetzner <metz@gmx.li>
2018-01-08 14:07:12 +0000

    added setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='ASSRUnit',
+      version='0.1',
+      packages=['assrunit','assrunit.Models'],
+      author='Christoph Metzner',
+      author_email='c.metzner@herts.ac.uk',
+      description='A module for computational models of auditory steady-state response (ASSR) deficits in schizophrenic patients',
+      license='CC BY 4.0',
+      url='https://github.com/ChristophMetzner/ASSRUnit',
+      )
